2e22242ffdf8049d6563bcb63cad1063c1258519
houssembenjemia <h.benjemia@atsu-studio.com>
2022-12-23 15:04:35 +0100

    ok

--- script.sh
+++ script.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
 cd /home/sparks/public_html 
-
 echo '------ workdir /home/sparks/public_html ----------'
-echo '------ artisan migrate ----------'
-
-php artisan migrate --no-interaction
-
-echo '------ artisan migrate succeded ----------'
-echo '------ artisan optimize ----------'
-
-php artisan optimize:clear
-
-echo '------ artisan optimize succeded ----------'
 
 echo '------ run composer install ----------'
-
 composer install --no-interaction
-
-echo '------ run dev ----------'
+echo '------ composer install succeded ----------'
 
 echo '------ run npm i ----------'
-
 npm install ;
+echo '------ npm install succeded ----------'
+
+
+echo '------ artisan migrate ----------'
+php artisan migrate --no-interaction
+echo '------ artisan migrate succeded ----------'
+
+
+echo '------ artisan optimize ----------'
+php artisan optimize:clear
+echo '------ artisan optimize succeded ----------'
 
 echo '------ run prod ----------'
-
 npm run prod ;
+
+
+
+
+
+
 
 
