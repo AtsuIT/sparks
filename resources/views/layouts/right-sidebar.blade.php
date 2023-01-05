67b833258d69804d5af074c796394c5be99245a6
@m.bahri <m.bahri@atsu-it.com>
2023-01-05 12:16:03 +0100

    fix translation

--- resources/views/layouts/right-sidebar.blade.php
+++ resources/views/layouts/right-sidebar.blade.php
@@ -103,14 +103,14 @@
                 </div>
             </div>
 
-                <h6 class="mt-4 mb-3">Direction</h6>
+                <h6 class="mt-4 mb-3 d-none">Direction</h6>
 
-                <div class="form-check form-check-inline">
+                <div class="form-check form-check-inline d-none">
                     <input class="form-check-input" type="radio" name="layout-direction"
                         id="layout-direction-ltr" value="ltr">
                     <label class="form-check-label" for="layout-direction-ltr">LTR</label>
                 </div>
-                <div class="form-check form-check-inline">
+                <div class="form-check form-check-inline d-none">
                     <input class="form-check-input" type="radio" name="layout-direction"
                         id="layout-direction-rtl" value="rtl">
                     <label class="form-check-label" for="layout-direction-rtl">RTL</label>
